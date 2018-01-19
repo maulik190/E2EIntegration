cc889c81e846831cf7704c873efd7abe7f747de3
Maulik <mauliks99@gmail.com>
2018-01-19 06:09:35 -0500

    First Commit

---
+++ E2ESeleniumJenkinsProject/src/testcases/TC_002.java
@@ -0,0 +1,30 @@
+package testcases;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class TC_002 {
+	
+	public static WebDriver driver;
+	
+	@Test
+	
+	public void testcase1(){
+		
+		
+				System.setProperty("webdriver.chrome.driver", System.getProperty("user.dir")+"//testLibrary//Chromdriver_mac//chromedriver");
+				driver = new ChromeDriver();
+				driver.manage().timeouts().implicitlyWait(30L, TimeUnit.SECONDS);
+		
+				driver.get("https://www.facebook.com/");
+				driver.findElement(By.xpath("//*[@id='email']")).sendKeys("Hello");
+				driver.findElement(By.xpath("//*[@id='pass']")).sendKeys("Hello");
+			//	driver.quit();
+
+	}
+
+}
